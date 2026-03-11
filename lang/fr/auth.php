1d837b39d797a11d6cbda819ce172e1e58b78af1
Fay019 <42018858+fay019@users.noreply.github.com>
2026-03-11 22:52:01 +0100

    Update translate auth

--- lang/fr/auth.php
+++ lang/fr/auth.php
@@ -26,7 +26,7 @@ return [
 
     // Messages
     'password_changed' => '✅ Mot de passe modifié avec succès !',
-    ‘password_must_change_note’ => "Vous ne pouvez pas accéder à l’application tant que vous n’avez pas changé votre mot de passe",
+    'password_must_change_note' => "Vous ne pouvez pas accéder à l’application tant que vous n’avez pas changé votre mot de passe",
     'password_must_change_notification' => '⚠️ Vous devez changer votre mot de passe avant de continuer.',
 
     // Auth form fields
@@ -38,21 +38,21 @@ return [
     'no_account' => 'Pas encore de compte ?',
 
     // Register
-    ‘name’ => ‘Nom’,
-    ‘confirm_password’ => ‘Confirmer le mot de passe’,
-    ‘already_registered’ => ‘Déjà inscrit ?’,
-    ‘have_account’ => ‘Vous avez déjà un compte ?’,
-    ‘register’ => "S’inscrire",
+    'name' => 'Nom',
+    'confirm_password' => 'Confirmer le mot de passe',
+    'already_registered' => 'Déjà inscrit ?',
+    'have_account' => 'Vous avez déjà un compte ?',
+    'register' => "S’inscrire",
 
     // Verify email
-    ‘verify_email_title’ => "Vérifier l’adresse email",
+    'verify_email_title' => "Vérifier l’adresse email",
     'verify_email_text' => 'Merci pour votre inscription ! Avant de commencer, veuillez vérifier votre adresse email en cliquant sur le lien que nous venons de vous envoyer.',
-    ‘verify_email_resent’ => "Un nouveau lien de vérification a été envoyé à l’adresse email fournie lors de l’inscription.",
-    ‘resend_button’ => "Renvoyer l’email de vérification",
-    ‘verification_success’ => "Merci d’avoir vérifié votre adresse email !",
+    'verify_email_resent' => "Un nouveau lien de vérification a été envoyé à l’adresse email fournie lors de l’inscription.",
+    'resend_button' => "Renvoyer l’email de vérification",
+    'verification_success' => "Merci d’avoir vérifié votre adresse email !",
 
     // Confirm password
     'confirm_password_title' => 'Confirmer le mot de passe',
-    ‘confirm_password_text’ => "Il s’agit d’une zone sécurisée de l’application. Veuillez confirmer votre mot de passe avant de continuer.",
+    'confirm_password_text' => "Il s’agit d’une zone sécurisée de l’application. Veuillez confirmer votre mot de passe avant de continuer.",
     'confirm_button' => 'Confirmer',
 ];

94251ad51945db300473626d3ab43813d581c86f
Fay019 <42018858+fay019@users.noreply.github.com>
2026-03-11 17:48:48 +0100

    - Correction d'un problème d'ordre d'exécution des migrations qui empêchait `php artisan migrate` de fonctionner. La migration de création de la table `social_links` est maintenant exécutée avant toute modification de celle-ci.

--- database/migrations/2026_03_01_160457_create_social_links_table.php
+++ database/migrations/2026_03_01_160457_create_social_links_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
             $table->string('url');
             $table->string('icon')->nullable();
             $table->integer('order')->default(0);
+            $table->boolean('is_visible')->default(true);
             $table->timestamps();
         });
     }
